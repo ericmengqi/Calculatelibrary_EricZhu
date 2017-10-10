6450d37b0554a182aeef524a00039e48a6e6d627
Eric Zhu <ericmengqi@gmail.com>
2017-10-10 09:00:33 -0700

    Started Quadratic Console Stuff

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -1,7 +1,13 @@
-
-public class Quadratic {
+import java.util.Scanner;
+ class Quadratic {
 
 	public static void QuadraticDescriber(double a, double b, double c) {
-   
-	}
+		System.out.println ("Welcome to Quadratic Describer");
+		System.out.println("Provide values for coefficients a, b, and c");
+		System.out.println(" ");
+		Scanner console = new Scanner(System.in);
+		double a1 = console.nextInt();
+		System.out.println("a:" + a1);
+		
+}
 }
