8da3f2de6b4eb28dba1ba7bc489ba6878b8f152d
ericm <ericm@DESKTOP-FFTHABH>
2017-10-16 08:37:53 -0700

    more work

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -8,11 +8,17 @@ import java.util.Scanner;
 		System.out.println("Provide values for coefficients a, b, and c");
 		System.out.println(" ");
 		console = new Scanner(System.in);
+		//User input "a" value
 		double a1 = console.nextInt();
 		System.out.println("a:" + a1);
+		//User input "b" value
 		double b1 = console.nextInt();
 		System.out.println("b:" + b1);
+		//User input "c" value
 		double c1 = console.nextInt();
 		System.out.println("c:" + c1);
+		System.out.println("Description of the graph:");
+		System.out.println("y = " + a1 + "x^2 " + b1 + "x " + c1);
+		
 }
 }
