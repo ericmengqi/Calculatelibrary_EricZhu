f63f575644bbc1b579ef9440aac3595795ef244b
ericm <ericm@DESKTOP-FFTHABH>
2017-10-16 00:15:43 -0700

    Worked on more.

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
  class Quadratic {
 
+	private static Scanner console;
+
 	public static void QuadraticDescriber(double a, double b, double c) {
 		System.out.println ("Welcome to Quadratic Describer");
 		System.out.println("Provide values for coefficients a, b, and c");
 		System.out.println(" ");
-		Scanner console = new Scanner(System.in);
+		console = new Scanner(System.in);
 		double a1 = console.nextInt();
 		System.out.println("a:" + a1);
-		
+		double b1 = console.nextInt();
+		System.out.println("b:" + b1);
+		double c1 = console.nextInt();
+		System.out.println("c:" + c1);
 }
 }
